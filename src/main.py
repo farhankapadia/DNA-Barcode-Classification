5356fc775e9ff0ee585faf4108d884ba60b2d629
Farhan Kapadia <52866509+farhankapadia@users.noreply.github.com>
2021-05-01 03:34:40 +0530

    preprocessing- label encoder

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import re
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
 
 algae= pd.read_csv('../data/algae.csv')
 algae['Family']= 'Algae'
@@ -20,7 +24,7 @@ fungi= pd.read_csv('../data/fungi.csv')
 fungi['Family']= 'Fungi'
 print(fungi.head())
 inga= pd.read_csv('../data/IngaTrain.csv')
-inga['Family']= 'IngaTrain'
+inga['Family']= 'Inga'
 print(inga.head())
 
 data= [algae, bats, cypra, dros, fish, fungi, inga]
@@ -40,9 +44,40 @@ combined.drop('index', axis=1, inplace=True)
 # print(combined['Properties'].describe())
 # print(combined['Sequence'].describe())
 # =============================================================================
+combined['Properties']= combined['Properties'].str.split('|').str[1]
+combined.rename(columns={'Properties':'Species'}, inplace=True)
+#combined = combined.sample(frac=1).reset_index(drop=True) #shuffling the dataframe
 print(combined.head())
 print(combined.tail())
 print(combined.shape)
 print(combined.describe())
 print(combined.info())
 print(combined.columns)
+
+#classifying only into species for now
+X= combined['Sequence']
+y= combined['Species']
+
+#label encoding the species name
+label_y= y.copy()
+label_encoder= LabelEncoder()
+label_y= label_encoder.fit_transform(y)
+print(label_y)
+
+#one hot encoding the sequence
+def string_to_array(my_string):
+    my_string = my_string.lower()
+    my_string = re.sub('[^acgt]', 'z', my_string)
+    my_array = np.array(list(my_string))
+    return my_array
+
+def one_hot_encoder(my_array):
+    label_encoder.fit(np.array(['a','c','g','t','z']))
+    integer_encoded = label_encoder.transform(my_array)
+    onehot_encoder = OneHotEncoder(sparse=False, dtype=int, n_values=5)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    onehot_encoded = np.delete(onehot_encoded, -1, 1)
+    return onehot_encoded
+
+label_X= X.copy()
